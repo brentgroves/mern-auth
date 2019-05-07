726b88c36158e7c0d8972247df87ded01f3e98e1
brent groves <brent.groves@gmail.com>
2019-05-07 19:13:07 -0400

    added more code

--- server.js
+++ server.js
@@ -19,5 +19,6 @@ mongoose
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there. 
+//Try Dokku which is a docker powered mini-Heroku
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));

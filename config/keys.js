4621be29ebd9a30cd09256c574669b01c4a94e12
brent groves <brent.groves@gmail.com>
2019-05-08 18:05:48 -0400

    added more code

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://brent:JesusLives1!@mycluster-yp9hk.mongodb.net/test?retryWrites=true" 
+  mongoURI: "mongodb+srv://brent:JesusLives1!@mycluster-yp9hk.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
 };

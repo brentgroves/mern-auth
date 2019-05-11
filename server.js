acb7274583cdaff1a59b345f3489f082843e7198
brent groves <brent.groves@gmail.com>
2019-05-11 19:17:30 -0400

    added more code

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
 const app = express();
+
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
+
 app.use(bodyParser.json());
+
 // DB Config
 const db = require("./config/keys").mongoURI;
+
 // Connect to MongoDB
 mongoose
   .connect(
@@ -19,6 +27,14 @@ mongoose
   )
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users",users);
+
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there. 
+
 //Try Dokku which is a docker powered mini-Heroku
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
